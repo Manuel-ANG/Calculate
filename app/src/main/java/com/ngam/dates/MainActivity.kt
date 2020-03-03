86eb7ba275f7654e8ced4a65b596c03fd2cd300d
Manuel-ANG <alejandro.4321@hotmail.com>
2020-03-03 12:33:04 -0600

    cambio version

--- app/src/main/java/com/ngam/dates/MainActivity.kt
+++ app/src/main/java/com/ngam/dates/MainActivity.kt
@@ -2,7 +2,6 @@ package com.ngam.dates
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ngam.calculatedates.Logic.CalculateDate
 
 class MainActivity : AppCompatActivity() {
 
